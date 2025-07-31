5d481c5c1769476cf8d117c4b5b76433ef832b10
Zain-The-Coder <zain015976@gmail.com>
2025-07-31 17:51:11 +0500

    First Project

---
+++ calculator - table - incrementation/app.js
@@ -0,0 +1,47 @@
+
+function addition () {
+    let x = parseInt(document.getElementById("secondNumber").value);
+    let y = parseInt(document.getElementById("secondNumber").value);
+    let output = " " ;
+    output += (x + y);
+    let printer = document.getElementById("output") ;
+    printer.innerHTML = "The Addition of " + x + " and " + y + " is : " + output ;
+}
+function substraction () {
+    let x = parseInt(document.getElementById("secondNumber").value);
+    let y = parseInt(document.getElementById("secondNumber").value);
+    let output = " " ;
+    output += (x - y);
+    let printer = document.getElementById("output") ;
+    printer.innerHTML = "The Substraction of " + x + " and " + y + " is : " + output ;
+}
+function multiplication () {
+    let x = parseInt(document.getElementById("secondNumber").value);
+    let y = parseInt(document.getElementById("secondNumber").value);
+    let output = " " ;
+    output += (x * y);
+    let printer = document.getElementById("output") ;
+    printer.innerHTML = "The Multiplication of " + x + " and " + y + " is : " + output ;
+}
+function division () {
+    let x = parseInt(document.getElementById("firstNumber").value);
+    let y = parseInt(document.getElementById("secondNumber").value);
+    let output = " " ;
+
+    output += (x / y);
+    let printer = document.getElementById("output") ;
+    printer.innerHTML = "The Division of " + x + " and " + y + " is : " + output ;
+}
+function remainder () {
+    let x = parseInt(document.getElementById("firstNumber").value);
+    let y = parseInt(document.getElementById("secondNumber").value);
+    let output = " " ;
+
+    output += (x % y);
+    let printer = document.getElementById("output");
+    printer.innerHTML = "The Remainder left is : " + output;
+}
+
+
+
+
