be48d9ce4491999e77c8ff1cbd2a703412cdaf1b
Zain-The-Coder <zain015976@gmail.com>
2025-08-02 14:55:26 +0500

    Third Project

--- Login/app.js
+++ Login/app.js
@@ -1,29 +1,44 @@
+
 let heading = document.getElementById("heading1");
 heading.innerHTML = "LOGIN APP" ;
+let output = document.getElementById("output");
 
-let userEmails = [] ;
 let userPasswords = [] ;
+let userEmails = [] ;
 
-function emailReceiver () {
-    let first = document.getElementById("formField1").value;
-    return first;   
+function newUser (usEm , usPm) {
+    let firstUserEmail = newUserEmail(usEm) ;
+    let firstUserPassword = newUserPassword(usPm);
+    oldUser();
+    if(firstUserEmail && firstUserPassword) {
+        userEmails.push(firstUserEmail);
+        userPasswords.push(firstUserPassword);
+        console.log(userEmails);
+        console.log(userPasswords);
+        output.innerHTML = " " ;
+    } else {
+        output.innerHTML = "Please Fill The Above Requirements !" ;
+    }
 }
 
-function passwordReceiver () {
-    let second = document.getElementById("formField2").value ;
-    return second ;
-
+function newUserEmail (usEm) {
+    usEm = document.getElementById("formField1").value;
+    return usEm ;
 }
-
-function newUser () {
-    let firstEmails = emailReceiver();
-    let firstPasswords = passwordReceiver () ;
-    userEmails.push(firstEmails);
-    userPasswords.push(firstPasswords);
-    console.log(userEmails);
-    console.log(userPasswords);
-}
+function newUserPassword (usPm) {
+    usPm = document.getElementById("formField2").value;
+    return usPm ;
+} 
 
 function oldUser () {
-
+    let oldUserEmail = document.getElementById("formField1").value;
+    let oldUserPassword = document.getElementById("formField2").value;
+    let emailChecker = userEmails.indexOf(oldUserEmail);
+    if(emailChecker !== -1 && userPasswords[emailChecker] === oldUserPassword) {
+        output.innerHTML = "WELCOME USER ! Login successfully."
+    } else if(emailChecker) {
+        output.innerHTML = "Authentication Failed ." ;
+    } else {
+        output.innerHTML = "Wrong Information !"
+    }
 }
