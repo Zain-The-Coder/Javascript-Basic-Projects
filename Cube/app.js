c58172bad70f31304398ba6771662243c21b4d63
Zain-The-Coder <zain015976@gmail.com>
2025-08-05 00:40:47 +0500

    JS Projects

---
+++ Cube/app.js
@@ -0,0 +1,70 @@
+// let first = '<div id="container"<div></div><div></div><div></div><div class="first"></div><div></div><div></div><div></div><div></div</div>';
+
+// let second = '<div id="container"<div></div><div></div><div></div><div class="first"></div><div></div><div class="first"></div><div></div><div></div><div></div</div>';
+
+// let third = ' <div id="container"><div class="first"></div><div></div><div></div><div></div><div class="first"></div><div></div><div></div><div></div><div class="first"></div</div>';
+
+// let fourth = '<div id="container"<div class="first"></div><div></div><div class="first"></div><div></div><div></div><div></div><div class="first"></div><div></div><div class="first"></div </div>'
+
+// let fifth = '<div id="container"<div class="first"></div><div></div> <div class="first"></div><div></div><div class="first"></div><div></div><div class="first"></div><div></div><div class="first"></div </div>'
+
+// let sixth = '<div id="container"><div class="first"></div><div></div><div class="first"></div><div class="first"></div><div></div><div class="first"></div><div class="first"></div><div></div><div class="first"></div</div>'
+
+
+
+
+// function play () {
+//     let y = document.getElementById("printer");
+//     let x = Math.ceil(Math.random() * 6) ;
+//     console.log(x)
+//         if(x === 1) {
+//         y.innerHTML = first ;
+//     } else if (x === 2) {
+//         y.innerHTML = second ;
+//     } else if (x === 3) {
+//         y.innerHTML = third ;
+//     } else if(x === 4) {
+//         y.innerHTML = fourth ;
+//     } else if (x === 5) {
+//         y.innerHTML = fifth;
+//     } else if(x === 6) {
+//         y.innerHTML = sixth;
+//     }
+// }
+
+
+
+
+
+//     var a = "a";
+//     let b = "b";
+//     const c = "c";
+// }
+// var a = "b";
+// var a = "b";
+// var a = "b";
+
+// function printName() {
+// }
+
+console.log(a);
+
+var a = "a";
+var a = "a";
+
+let b = "a";
+let b = "a";
+
+const c = "b";
+const c = "b";
+var a = "a";
+a = "a";
+
+let b = "a";
+b = "a";
+
+const c = "b";
+c = "b";
+
+
+console.log(typeof {});
--- Login/app.js
+++ Login/app.js
@@ -9,16 +9,18 @@ let userEmails = [] ;
 function newUser (usEm , usPm) {
     let firstUserEmail = newUserEmail(usEm) ;
     let firstUserPassword = newUserPassword(usPm);
+
     oldUser();
     if(firstUserEmail && firstUserPassword) {
         userEmails.push(firstUserEmail);
         userPasswords.push(firstUserPassword);
         console.log(userEmails);
         console.log(userPasswords);
-        output.innerHTML = " " ;
+        output.innerHTML = "The New User Has Been Added" ;
     } else {
         output.innerHTML = "Please Fill The Above Requirements !" ;
     }
+
 }
 
 function newUserEmail (usEm) {
@@ -42,3 +44,40 @@ function oldUser () {
         output.innerHTML = "Wrong Information !"
     }
 }
+
+// let saveData = localStorage.getItem("username");
+// if(saveData) {
+//     document.getElementById("output").innerHTML = "Welcome Back " + saveData + " !";
+// }
+// function saveName () {
+//     let x = document.getElementById("usernameInput").value;
+//     if(x) {
+//         localStorage.setItem("username" , x);
+//         document.getElementById("output").innerHTML = "Welcome " + x + " your name has been saved !";
+//         document.getElementById("usernameInput").value = " " ;
+//     } else {
+//         document.getElementById("output").innerHTML = "Invalid Input !";
+//     }
+// }
+
+// let saveName = localStorage.getItem("userName");
+// let saveNotes = localStorage.getItem("userNote");
+
+// if(saveName && saveNote) {
+//     document.getElementById("output").innerHTML = "THE OLD USER IS : " + saveName + "<br>" + "YOUR NOTE : " + saveNotes ;
+// }
+
+// function saveNote () {
+//     let x = document.getElementById("userName").value;
+//     let y = document.getElementById("userNote").value;
+//     if(x && y) {
+//         document.getElementById("output").innerHTML = "Welcome " + x + " your name and note has been saved."
+//         localStorage.setItem("userName" , x);
+//         localStorage.setItem("userNote" , y);
+//         document.getElementById("userName").value = " " ;
+//         document.getElementById("userNote").value = " " ;
+//     } else {
+//         document.getElementById("output").innerHTML = "Please Fill All Requirements !";
+//     }
+// }
+
