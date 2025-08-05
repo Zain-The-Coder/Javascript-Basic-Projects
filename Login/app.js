f6f1b13b3a4ac92a65e2420e690e13a11cc78856
Zain-The-Coder <zain015976@gmail.com>
2025-08-06 01:07:52 +0500

    Login Page

--- Login/app.js
+++ Login/app.js
@@ -2,47 +2,55 @@
 
 
 
-let output = document.getElementById("output");
-let userEmails = [ ] ;
-let userPasswords = [ ] ;
+// let output = document.getElementById("output");
+// let userEmails = [ ] ;
+// let userPasswords = [ ] ;
 
-function newUser () {
-    oldUser()
-    let emailOfUser = takingEmail () ;
-    let passwordOfUser = takingPassword () ;
-    if(emailOfUser && passwordOfUser) {
-        let userinfo = {
-            email : emailOfUser , 
-            password : passwordOfUser ,
-        }
-        userEmails.push(userinfo.email);
-        userPasswords.push(userinfo.password);
-        console.log(userinfo);
-        output.innerHTML = "The New User Has Been Entered !" ;
-    } else {
-        output.innerHTML = "Please fill The Above Requirements !" ;
-    }
-}
+// function newUser () {
 
-function takingEmail () {
-    let userEmail = document.getElementById("formField1").value ;
-    return userEmail ;
-}
-function takingPassword () {
-    let userPassword = document.getElementById("formField2").value ;
-    return userPassword ;
-}
+//     let emailOfUser = takingEmail () ;
+//     let passwordOfUser = takingPassword () ;
+//     if(userEmails.includes(emailOfUser)) {
+//         output.innerHTML = "This Email Is Already Registered !" ;
+//         return ;
+//     }
+//     if(emailOfUser && passwordOfUser) {
+//         let userinfo = {
+//             email : emailOfUser , 
+//             password : passwordOfUser ,
+//         }
+//         userEmails.push(userinfo.email);
+//         userPasswords.push(userinfo.password);
+//         console.log(userinfo);
+//         output.innerHTML = "The New User Has Been Entered !" ;
+//     } else {
+//         output.innerHTML = "Please fill The Above Requirements !" ;
+//     }
+// }
 
-function oldUser () {
-    let oldUserEmail = document.getElementById("formField1").value ;
-    let oldUserPassword = document.getElementById("formField2").value ;
-    let matchedEmail = userEmails.indexOf(oldUserEmail) ;
-    if(matchedEmail !== -1 && userPasswords[matchedEmail] === oldUserPassword) {
-        output.innerHTML = "Welcome "+ oldUserEmail + " ! Login successfully." ;
-    } else if(matchedEmail !== -1) {
-        output.innerHTML = "Authentication Failed !" ;
-    } else {
-        output.innerHTML = "Invaild Input !"
-    }
+// function takingEmail () {
+//     let userEmail = document.getElementById("formField1").value ;
+//     return userEmail ;
+// }
+// function takingPassword () {
+//     let userPassword = document.getElementById("formField2").value ;
+//     return userPassword ;
+// }
 
-}
+// function oldUser () {
+//     let oldUserEmail = document.getElementById("formField1").value ;
+//     let oldUserPassword = document.getElementById("formField2").value ;
+//     let matchedEmail = userEmails.indexOf(oldUserEmail) ;
+//     if(matchedEmail !== -1 && userPasswords[matchedEmail] === oldUserPassword) {
+//         output.innerHTML = "Welcome "+ oldUserEmail + " ! Login successfully." ;
+//     } else if(matchedEmail !== -1) {
+//         output.innerHTML = "Authentication Failed !" ;
+//     } else {
+//         output.innerHTML = "Invaild Input !"
+//     }
+
+// }
+
+
+
+
