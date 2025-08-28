1a18e2537d99065e84fe55e1d3eecc1befb61e24
Zain-The-Coder <zain015976@gmail.com>
2025-08-28 16:47:21 +0500

    Play Card Game

--- Play Cards/app.js
+++ Play Cards/app.js
@@ -2,25 +2,20 @@ let firstImage = document.getElementById("firstImage");
 let secondImage = document.getElementById("secondImage");
 let thirdImage = document.getElementById("thirdImage");
 let message = document.getElementById("message");
-let isFlipped = false ; 
+let generateNumber = Math.ceil(Math.random() * 3);
+console.log(generateNumber);
+let isFlipped = false ;     
 
-const changer = (userNumber) => {
-    
+const changer1 = (userNumber) => {
     if(!isFlipped) {
-        let generateNumber = Math.ceil(Math.random() * 3);
-        console.log(generateNumber)
     if(generateNumber === 1) {
         firstImage.src = "./Images/king-card.jpg" ;
-        secondImage.src = "./Images/joker.webp" ;
-        thirdImage.src = "./Images/joker.webp" ;
-    } else if (generateNumber === 2) {
+    } 
+    else if (generateNumber === 2) {
         firstImage.src = "./Images/joker.webp" ;
-        secondImage.src = "./Images/king-card.jpg" ;
-        thirdImage.src = ".//Images/joker.webp" ;
-    } else if(generateNumber === 3) {
+    }
+     else if(generateNumber === 3) {
         firstImage.src = "./Images/joker.webp" ;
-        secondImage.src = "./Images/joker.webp" ;
-        thirdImage.src = "./Images/king-card.jpg" ;
     }
     isFlipped = true ;
     if(userNumber === generateNumber) {
@@ -33,6 +28,57 @@ const changer = (userNumber) => {
         message.style.color = "red";
     }
 }
+}
+
+const changer2 = (userNumber) => {
+    if(!isFlipped) {
+    if(generateNumber === 1) {
+        secondImage.src = "./Images/joker.webp" ;
+    } 
+    else if(generateNumber === 2) {
+        secondImage.src = "./Images/king-card.jpg" ;
+    } 
+    else if(generateNumber === 3) {
+        secondImage.src = "./Images/joker.webp" ;
+    }
+    isFlipped = true ;
+    if(userNumber === generateNumber) {
+        message.innerHTML = "🎉 You Guessed Right ! " + "<br>" + "Congratulation , You Won the Game ." ;
+        message.style.color = "green"
+       // message.style.fontFamily = "poppins"
+
+    } else {
+        message.innerHTML = "❌ Better Luck Next Time ! " + "<br>" + "You Lost ."
+        message.style.color = "red";
+    }
+
+}
+}
+
+const changer3 = (userNumber) => {
+    if(!isFlipped) {
+    if(generateNumber === 1) {
+        thirdImage.src = "./Images/joker.webp" ;
+    } 
+    else if(generateNumber === 2) {
+        thirdImage.src = "./Images/joker.webp" ;
+    }
+    else if(generateNumber === 3) {
+        thirdImage.src = "./Images/king-card.jpg" ;
+    }
+    isFlipped = true ;
+
+    if(userNumber === generateNumber) {
+        message.innerHTML = "🎉 You Guessed Right ! " + "<br>" + "Congratulation , You Won the Game ." ;
+        message.style.color = "green"
+       // message.style.fontFamily = "poppins"
+
+    } else {
+        message.innerHTML = "❌ Better Luck Next Time ! " + "<br>" + "You Lost ."
+        message.style.color = "red";
+    }
+}
+
 }
 
 const reset = () => {
