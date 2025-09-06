4f569e48103d5c9dd919c44c885b4d94abfa1ece
Zain-The-Coder <zain015976@gmail.com>
2025-09-06 13:10:42 +0500

    Crud Application

--- Crud Application/app.js
+++ Crud Application/app.js
@@ -4,6 +4,8 @@ let changerdiv = document.getElementById("curdDiv");
 
 
 function addItem () {
+    let shortDiv = document.createElement("div");
+    changerdiv.appendChild(shortDiv);
 if(userInput.value === "") {
     message.innerHTML = "Input Field Is Empty !" ;
     message.style.color = "red" ;
@@ -17,10 +19,14 @@ if(userInput.value === "") {
     let h2Txt = document.createTextNode("Remaining Task");
     taskHead.appendChild(h2Txt);
     taskDiv.appendChild(taskHead);
+
     
     let divLi = document.createElement("li");
+    divLi.style.color = "bisque" ;
     let liTxt = document.createTextNode(userInput.value);
     divLi.appendChild(liTxt) ;
+    divLi.style.marginTop = "-15px" ;
+    divLi.style.marginBottom = "5px"
 
     let buttonDiv = document.createElement("div");
     let editButton = document.createElement("button");
@@ -34,9 +40,54 @@ if(userInput.value === "") {
     buttonDiv.appendChild(editButton) ;
     buttonDiv.appendChild(removeButton) ;
     buttonDiv.appendChild(completeButton);
+
+    completeButton.style.color = "black" ;
+    completeButton.style.backgroundColor = "lightblue" ;
+    completeButton.style.height = "29px" ;
+    completeButton.style.width = "100px" ;
+    completeButton.style.borderRadius = "5px" ;
+    completeButton.style.fontSize = "14px" ; 
+    completeButton.style.padding = "0px"
+
     
-    changerdiv.appendChild(taskDiv);
-    changerdiv.appendChild(divLi);
-    changerdiv.appendChild(buttonDiv);
+    removeButton.style.color = "black" ;
+    removeButton.style.backgroundColor = "red" ;
+    removeButton.style.height = "29px" ;
+    removeButton.style.width = "25px" ;
+    removeButton.style.borderRadius = "5px" ;
+    removeButton.style.fontSize = "14px" ;
+    removeButton.style.padding = "0px" ;
+    removeButton.style.width = "130px" ;
+    removeButton.style.marginLeft = "10px" ;
+    removeButton.style.marginRight = "10px"
+
+    
+    editButton.style.color = "black" ;
+    editButton.style.backgroundColor = "green" ;
+    editButton.style.height = "29px" ;
+    editButton.style.width = "25px" ;
+    editButton.style.borderRadius = "5px" ;
+    editButton.style.fontSize = "14px" ;
+    editButton.style.padding = "0px" ;
+    editButton.style.width = "100px" ;
+
+
+    
+    shortDiv.appendChild(taskDiv);
+    shortDiv.appendChild(divLi);
+    shortDiv.appendChild(buttonDiv);
+
+    shortDiv.style.border = "2.5px solid gold" ;
+    shortDiv.style.width = "80%"
+    shortDiv.style.margin = "auto" ;
+    shortDiv.style.padding = "10px" ;
+    shortDiv.style.marginBottom = "20px" ;
+    shortDiv.style.marginTop = "20px" ;
+    shortDiv.style.borderRadius = "20px"
+    taskHead.style.color = "red" ;
+    taskHead.style.marginTop = "-5px"
+ 
+
+
 }
 }
