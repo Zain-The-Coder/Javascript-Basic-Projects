caca511d2cec7e2dcfe165254f237089f3cd5e66
Zain-The-Coder <zain015976@gmail.com>
2025-12-06 02:33:14 +0500

    Login Page , Pure Logic

--- Quiz Application/Login/login.js
+++ Quiz Application/Login/login.js
@@ -1,39 +1,62 @@
+
+//For Dom Working Variables
 let errorMessanger = document.getElementById("print");
 let user_name = document.getElementById("formField1");
 let user_email = document.getElementById("formField2");
 let user_password = document.getElementById("formField3");
 
+//Global Declarations
+let userName;
+let userEmail;
+let userPassword;
+
+//Error Checker Variables
+let atCount = 0 ;
+let error = false ;
+
 
 function signUp () {
-    errorDetector();
+    errorHandler();
 }
 
-function errorDetector () {
-    let userName = user_name.value;
-    let userEmail = user_email.value ;
-    let userPassword = user_password.value ;
 
 
-     if(!userName || !userEmail || !userPassword) {
-        errorMessanger.innerHTML = "Please Fill The Above Required Fields !" ;
-        errorMessanger.style.color = "red" ;
-        errorMessanger.style.fontWeight = "bold";
-        return ;
-    }
 
+function errorHandler () {
+    //Value Takers
+    userName = user_name.value ;
+    userEmail = user_email.value ;
+    userPassword = user_password.value ;
+
+    //Email @ Check
     for(let i = 0 ; i < userEmail.length ; i++) {
-        if(userEmail[i] !== "@") {
-            errorMessanger.innerHTML = "Please Enter Valid Email !" ;
-            errorMessanger.style.color = "red" ;
-            errorMessanger.style.fontWeight = "bold";
+        if(userEmail[i] === "@") {
+            atCount++ ;
         }
-        return ;
     }
 
+    //Email Spaces Check
+    for(let i = 0 ; i < userEmail.length ; i++) {
+        if(userEmail[i] === " ") {
+            error = true ;
+        }
+    }
+
+    //Error Messages 
+    if(!userName && !userEmail && !userPassword) {
+        errorMessanger.innerHTML = "Fill The Above All Requirements !" ;
+        return ;
+    }
     if(userPassword.length < 6) {
-        errorMessanger.innerHTML = "Password Must Be 6 Characters !";
-        errorMessanger.style.color = "red" ;
-        errorMessanger.style.fontWeight = "bold";
+        errorMessanger.innerHTML = "Password Should Be Strong or 6 Letters !" ;
+        return ;
+    }
+    if(atCount !== 1) {
+        errorMessanger.innerHTML = "Please Enter Correct Email !" ;
+        return ;
+    }
+    if(error = true) {
+        errorMessanger.innerHTML = "Remove Extra Spaces From Email" ;
         return ;
     }
 }
